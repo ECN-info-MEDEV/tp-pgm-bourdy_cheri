8b3bf1cf3a84e401e8c66fb56a28dc1e9ccb227e
Aym <aymericb5@gmail.com>
2022-01-06 10:58:01 +0100

    Fix issues detected by sonar qube

--- src/main/java/edu/ecn/medev2021/PgmGenerator.java
+++ src/main/java/edu/ecn/medev2021/PgmGenerator.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.Random;
 
 public class PgmGenerator {
+    private final Random random = new Random();
     /**
      * Generates a random pgm image (a matrice with values between 0 and 255)
      *
@@ -15,11 +16,9 @@ public class PgmGenerator {
      */
     public int[][] generatePgmImage(int width, int height) {
         int[][] img = new int[height][width];
-        Random rand = new Random();
-
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                img[i][j] = rand.nextInt(256);
+                img[i][j] = random.nextInt(256);
             }
         }
         return img;
@@ -34,28 +33,26 @@ public class PgmGenerator {
      */
     public void writePgmImage(File writeTo, int[][] image) throws IOException {
         writeTo.createNewFile();
-        FileWriter writer = new FileWriter(writeTo);
-        writer.write("P2\n");
-        writer.write("# \n");
-        writer.write(image[0].length + "  " + image.length+"\n");
-        writer.write("255\n");
-        int lineSize = 0;
-        int c = 0;
-        for (int[] line : image) {
-            for (int pixel : line) {
-                if (lineSize > 0) {
-                    writer.write("  " + pixel);
-                } else {
-                    writer.write(""+pixel);
-                }
-                c++;
-                lineSize++;
-                if (lineSize == 10) {
-                    writer.write(" \n");
-                    lineSize = 0;
+        try (FileWriter writer = new FileWriter(writeTo)) {
+            writer.write("P2\n");
+            writer.write("# \n");
+            writer.write(image[0].length + "  " + image.length + "\n");
+            writer.write("255\n");
+            int lineSize = 0;
+            for (int[] line : image) {
+                for (int pixel : line) {
+                    if (lineSize > 0) {
+                        writer.write("  " + pixel);
+                    } else {
+                        writer.write("" + pixel);
+                    }
+                    lineSize++;
+                    if (lineSize == 10) {
+                        writer.write(" \n");
+                        lineSize = 0;
+                    }
                 }
             }
         }
-        writer.close();
     }
 }
--- src/main/java/edu/ecn/medev2021/TP3Medev.java
+++ src/main/java/edu/ecn/medev2021/TP3Medev.java
@@ -1,33 +1,29 @@
 package edu.ecn.medev2021;
 
-import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.logging.Logger;
 
 /**
  * A simple test for the pgm code
  */
 public class TP3Medev {
+    public static final Logger log = Logger.getLogger("PgmGenerator");
+    
     public static void main(String[] args) {
         PgmReader reader = new PgmReader();
         try {
             File file = new File("baboon.pgm");
-            System.out.println("File " + file.getAbsolutePath());
+            TP3Medev.log.info("File " + file.getAbsolutePath());
             int[][] img = reader.readPgmFile(file);
             int[] histogram = reader.getHistogram(img);
-            System.out.println(Arrays.toString(histogram));
+            TP3Medev.log.info(() -> Arrays.toString(histogram));
 
             PgmGenerator generator = new PgmGenerator();
             int[][] img2 = generator.generatePgmImage(256, 256);
-            System.out.println(Arrays.toString(reader.getHistogram(img2)));
+            TP3Medev.log.info(() -> Arrays.toString(reader.getHistogram(img2)));
             generator.writePgmImage(new File("test1.pgm"), img2);
-
-            /*file = new File("test1.pgm");
-            System.out.println("File "+file.getAbsolutePath());
-            img = reader.readPgmFile(file);
-            histogram = reader.getHistogram(img);
-            System.out.println(Arrays.toString(histogram));*/
 
             img = reader.difference(img2, img);
             displayImage(img);
